9911eb08ad387ee7ac66ff5398415cd85ffb6ddd
jey-uki <srjey.uki@gmail.com>
2025-10-16 14:51:36 +0530

    HomeCard component created

---
+++ src/componensts/HomeCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function HomeCard({url}) {
+  return (
+    <>
+      <div className="flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 mb-3 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            <Link to={url}>{url.toUpperCase()}</Link>
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            This is {url} page
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,10 @@
-import { Link } from "react-router-dom";
+import HomeCard from "../componensts/HomeCard";
 
 function Home() {
   return (
     <>
-      <div className="flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 mb-3 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div>
-          <div className="text-xl font-medium text-black dark:text-white">
-            <Link to={"/about"}>About</Link>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            this is about page ragring our company
-          </p>
-        </div>
-      </div>
-
-      <div className="flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div>
-          <div className="text-xl font-medium text-black dark:text-white">
-            <Link to={"/contact"}>Contact</Link>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            this is contact ragring our company
-          </p>
-        </div>
-      </div>
+      <HomeCard url="about" />
+      <HomeCard url="contact" />
     </>
   );
 }
