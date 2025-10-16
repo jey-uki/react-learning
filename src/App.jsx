68dceb563e4af31f497b04376b4c3fce5595eed3
jey-uki <srjey.uki@gmail.com>
2025-10-16 12:55:57 +0530

    tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="main">
+      <div className="px-6">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
-      <div className="main">
-        <h1>Home page</h1>
+      <div className="flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 mb-3 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            <Link to={"/about"}>About</Link>
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            this is about page ragring our company
+          </p>
+        </div>
+      </div>
+
+      <div className="flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            <Link to={"/contact"}>Contact</Link>
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            this is contact ragring our company
+          </p>
+        </div>
       </div>
     </>
   );
