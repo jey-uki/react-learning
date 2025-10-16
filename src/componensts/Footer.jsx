74aa44a447b43afa9df7dae4e9d1df6fdf3e094f
jey-uki <srjey.uki@gmail.com>
2025-10-16 15:19:01 +0530

    header and footer style added

--- src/componensts/Footer.jsx
+++ src/componensts/Footer.jsx
@@ -1,11 +1,26 @@
 function Footer() {
   return (
     <>
-      <div className="main">
+      <div style={style.container}>
         <h1>Footer components</h1>
       </div>
     </>
   );
 }
+
+const style = {
+  container: {
+    backgroundColor: "#1d293d",
+    height: "50px",
+    color: "white",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: "0",
+    width: "100%"
+  },
+};
 
 export default Footer;
--- src/componensts/Header.jsx
+++ src/componensts/Header.jsx
@@ -1,11 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <>
-      <div className="main">
-        <h1>Header components</h1>
+      <div style={style.container}>
+        <Link to="/">
+          <h1>Header components</h1>
+        </Link>
       </div>
     </>
   );
 }
+
+const style = {
+  container: {
+    backgroundColor: "#1d293d",
+    height: "50px",
+    marginBottom: "20px",
+    color: "white",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
 
 export default Header;
