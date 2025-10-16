bbc2976d8aa9f62d744500a1f9723d6bd9eb979c
jey-uki <srjey.uki@gmail.com>
2025-10-16 12:26:55 +0530

    paged link with home

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Header from "./componensts/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Header />
       <div className="main">
-        <Header />
-        <Home />
-        <About />
-        <Contact />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
+      <Footer />
     </>
   );
 }
